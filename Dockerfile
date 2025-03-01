0ff60d99e47230db7c5972375f81ede0675188d8
Sean Sell <rough68fish@yahoo.com>
2025-03-01 14:59:39 -0500

    simple version that talks to a local ollama model

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM node:lts
 WORKDIR /app
+COPY package*.json ./
+# Install app dependencies
+RUN npm install
 COPY demo_app.js ./
-RUN npm i readline
 EXPOSE 8080
 CMD ["node", "demo_app.js"]
--- demo_app.js
+++ demo_app.js
@@ -1,12 +1,26 @@
-const readline = require('readline');
+import { Ollama } from 'ollama';
+import readline from 'readline';
+import util from 'util';
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question('Question: ', (question) => {
+rl.question('Question: ', async (question) => {
   rl.close();
 
-  const answer = `The answer to ${question} is: 42`;
-  console.log(answer);
+  try {
+    const ollama = new Ollama({ host: 'http://host.docker.internal:11434' });
+    const response = await ollama.chat({
+        model: 'nous-hermes2',
+        messages: [{ role: 'user', content: question }],
+      })
+    
+    console.log('Full response:', util.inspect(response, { showHidden: false, depth: null, colors: true }));
+    const answer = response.message.content;
+    console.log(`The answer to ${question} is: \n ${answer}`);
+  } catch (error) {
+    console.error('Error communicating with Ollama:', error);
+  }
 });
