748653e9c5b4119175955a814abd2964e2b30fc8
Sean Sell <rough68fish@yahoo.com>
2025-03-01 22:37:41 -0500

    web form version of the simple chat

--- demo_app.js
+++ demo_app.js
@@ -1,26 +1,45 @@
 import { Ollama } from 'ollama';
-import readline from 'readline';
-import util from 'util';
+import express from 'express';
+import bodyParser from 'body-parser';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <form action="/ask" method="post">
+      <label for="question">Question:</label>
+      <input type="text" id="question" name="question">
+      <button type="submit">Ask</button>
+    </form>
+  `);
 });
 
-rl.question('Question: ', async (question) => {
-  rl.close();
+app.post('/ask', async (req, res) => {
+  const question = req.body.question;
 
   try {
     const ollama = new Ollama({ host: 'http://host.docker.internal:11434' });
     const response = await ollama.chat({
-        model: 'nous-hermes2',
-        messages: [{ role: 'user', content: question }],
-      })
-    
-    console.log('Full response:', util.inspect(response, { showHidden: false, depth: null, colors: true }));
+      model: 'nous-hermes2',
+      messages: [{ role: 'user', content: question }],
+    });
     const answer = response.message.content;
-    console.log(`The answer to ${question} is: \n ${answer}`);
+    res.send(`
+      <p>The answer to "${question}" is:</p>
+      <p>${answer}</p>
+      <a href="/">Ask another question</a>
+    `);
   } catch (error) {
-    console.error('Error communicating with Ollama:', error);
+    res.send(`
+      <p>Error communicating with Ollama: ${error}</p>
+      <a href="/">Try again</a>
+    `);
   }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
